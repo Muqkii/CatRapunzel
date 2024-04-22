c192bc683745760e68670527141a71603b93780a
Muqkii <chloepunt42@gmail.com>
2024-04-22 16:34:48 +0200

    bomb script done
    info van hoe het werkt staat in notes in het script

---
+++ Explosion.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosionagain : MonoBehaviour
+{
+    public float radius;
+    public float force;
+    bool kaboom;
+
+
+    void Update()
+    {
+        keyTrigger();
+
+        if (kaboom) //if 'e' is pressed
+        {
+            knockBack(); // call knockback
+            Destroy(gameObject); //destroy bomb
+
+            kaboom = false; //stop it from repeating
+        }
+    }
+
+    void knockBack()
+    {
+        Collider[] collider = Physics.OverlapSphere(transform.position, radius); //search for gameobjects in the blast radius
+
+        foreach(Collider nearbystuff in collider) //search in every object that is found
+        {
+            Rigidbody rb = nearbystuff.GetComponent<Rigidbody>(); //do they have rigidbodies?
+            if(rb != null) //if they do
+            {
+                rb.AddExplosionForce(force, transform.position, radius); //then boom
+            }
+        }
+    }
+
+    void keyTrigger()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) //check if 'e' is pressed
+        {
+            kaboom = true;
+        }
+    }
+}
